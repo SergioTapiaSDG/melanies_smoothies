cf1f03284d2d75e829dc3e33669181abe281f181
SergioTapiaSDG <sergio.tapia@sdggroup.com>
2026-06-26 10:58:36 +0200

    Uncomment smoothiefroot API request
    Uncomment the API request for watermelon data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import os
 from snowflake.snowpark.functions import col
 import requests
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+#smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
